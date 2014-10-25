ef8ff883a6dde8605b8855cb7e7b2d8d7b32244e
Melissa <md9645@att.com>
2014-10-25 10:26:05 -0500

    Programming Assignment 2 commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the value of the mean so that when we need it again
+## it can be looked up rather than recomputed
 
-## Write a short comment describing this function
+## Create a special matrix and cache its inverse calculation
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y)  {
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setmean<-function(mean) m<<-mean
+  getmean<-function()m
+  list(set=set, get=get,
+       setmean=setmean,
+       getmean=getmean)
 }
 
-
-## Write a short comment describing this function
+## Computes or retrieves the inverse of special matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  m<-x$getmean()
+  if(!is.null(m))   {
+        message("getting cached data")
+        return(m)
+  }
+  data<-x$get()
+  m<-mean(data, ...)
+  x$setmean(m)
+  m
+}  
+
+## Returns a matrix that is the inverse of 'x'
+
+solve(x)
+
